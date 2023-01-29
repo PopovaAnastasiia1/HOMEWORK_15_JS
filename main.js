20e8ca8f193b6e45baa4749f5e4f5ba04e05eb3e
PopovaAnastasia1 <popovaanastasiia123@gmail.com>
2023-01-29 02:06:32 +0200

    git commit -m

---
+++ main.js
@@ -0,0 +1,10 @@
+num = +prompt("Введіть число");
+while (num === "" || Number(isNaN(num))) {
+  num = +prompt("Введіть  число ще раз:");
+}
+
+function factorial(n) {
+  return n != 1 ? n * factorial(n - 1) : 1;
+}
+
+console.log(factorial(num));
